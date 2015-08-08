5fe8b0fb15cfb02d1b8bec2591df497f952894ff
thexilent13 <jennicaalcalde@yahoo.com>
2015-08-08 18:33:10 +0800

    Initial commit

---
+++ app/src/main/java/com/jennica/apps/beeradviser/FindBeerActivity.java
@@ -0,0 +1,46 @@
+package com.jennica.apps.beeradviser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class FindBeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_find_beer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickFindBeer(View view) {
+        TextView brands = (TextView) findViewById(R.id.txtView_brands);
+        Spinner color = (Spinner) findViewById(R.id.spinner_color);
+        brands.setText(String.valueOf(color.getSelectedItem()));
+    }
+}
